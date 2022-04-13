7a16393b48bfaa59e212dc14b56c47bb9c26688a
ayumu-1212 <ayumuabe1434@gmail.com>
2022-04-13 23:10:44 +0900

    :bookmark: ひとまずコード記載

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
+    _ "github.com/go-sql-driver/mysql"
+		"GinTutorial2/controller"
+		"GinTutorial2/middleware"
 )
 
-func main() {
+func main(){
 	engine := gin.Default()
-	engine.Static("/static", "./static")
+	// ミドルウェア
+	engine.Use(middleware.RecordUaAndTime)
+	// CRUD 書籍
+	bookEngine := engine.Group("/book") 
+	{
+		v1 := bookEngine.Group("v1")
+		{
+			v1.POST("/add", controller.BookAdd)
+			v1.GET("/list", controller.BookList)
+			v1.PUT("/update", controller.BookUpdate)
+			v1.DELETE("/delete", controller.BookDelete)
+		}
+	}
 	engine.Run(":3000")
 }
